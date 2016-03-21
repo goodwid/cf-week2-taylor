03a95d4cfabecb6ac536e54942b3b3c81a43612c
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-21 14:34:06 -0700

    Defined multiply() in compute.js and updated app.js with multiply() test info.

--- gulp-mon-lab/js/app.js
+++ gulp-mon-lab/js/app.js
@@ -1,6 +1,6 @@
 var pFunctionIn  = document.getElementById('pFunctionIn');
 var pFunctionOut = document.getElementById('pFunctionOut');
 
-var arg = '4, 5';
-pFunctionIn.textContent  = 'Calling sum() with an argument of ' + arg;
-pFunctionOut.textContent = 'sum() returned ' + sum(4,5);
+var arg = '8, 3';
+pFunctionIn.textContent  = 'Calling multiply() with an argument of ' + arg;
+pFunctionOut.textContent = 'multiply() returned ' + multiply(8,3);
--- gulp-mon-lab/js/compute.js
+++ gulp-mon-lab/js/compute.js
@@ -41,3 +41,10 @@ function sum(num1, num2) {
   console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total);
 }
 exports.sum = sum;
+
+function multiply(num1, num2) {
+  var total = (num1 * num2);
+  return total;
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + total);
+}
+exports.multiply = multiply;
