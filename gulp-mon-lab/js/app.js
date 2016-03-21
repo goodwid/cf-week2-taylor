cc4803e2659d8d2ec0e785f8a58c687f6da57068
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-21 16:21:29 -0700

    Created questions array in app.js

--- gulp-mon-lab/js/app.js
+++ gulp-mon-lab/js/app.js
@@ -13,6 +13,14 @@ var pA1 = document.getElementById('answer-5');
 var pQ1  = document.getElementById('question-6');
 var pA1 = document.getElementById('answer-6');
 
+var questions = [
+  'Write a function called sum() that takes in two numbers as arguments and then returns the sum of those numbers.',
+  'Write a function called multiply() that takes in two numbers as arguments and then returns the product of those numbers.',
+  'Write a function called sumAndMultiply() that takes in THREE numbers as arguments and then returns two values in an array: the sum of those numbers and the product of those numbers.',
+  'Write a function called sumArray() that takes in an array of numbers as a single argument and then returns the sum of those numbers.',
+  'Write a function called multiplyArray() that takes in an array of numbers as a single argument and then returns the product of those numbers.'
+]
+
 
 var arg = '2, 3, 4, 5';
 document.addEventListener('DOMContentLoaded', function() {
