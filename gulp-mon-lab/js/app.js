1c95c8eaa41a3e4485f29b4e1f8f5e8c58dbedb1
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-21 16:13:10 -0700

    Created variables in app.js that represent <p> elements

--- gulp-mon-lab/js/app.js
+++ gulp-mon-lab/js/app.js
@@ -1,7 +1,18 @@
 var exports = {};
 
-var pFunctionIn  = document.getElementById('pFunctionIn');
-var pFunctionOut = document.getElementById('pFunctionOut');
+var pQ1  = document.getElementById('question-1');
+var pA1 = document.getElementById('answer-1');
+var pQ1  = document.getElementById('question-2');
+var pA1 = document.getElementById('answer-2');
+var pQ1  = document.getElementById('question-3');
+var pA1 = document.getElementById('answer-3');
+var pQ1  = document.getElementById('question-4');
+var pA1 = document.getElementById('answer-4');
+var pQ1  = document.getElementById('question-5');
+var pA1 = document.getElementById('answer-5');
+var pQ1  = document.getElementById('question-6');
+var pA1 = document.getElementById('answer-6');
+
 
 var arg = '2, 3, 4, 5';
 document.addEventListener('DOMContentLoaded', function() {
