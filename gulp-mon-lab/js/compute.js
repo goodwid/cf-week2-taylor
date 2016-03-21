b3d9b76baec5be2aa7e5a6f993739070092b2550
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-21 14:50:31 -0700

    Defined sumAndMultiply() in compute.js and updated app.js with multiply() test info. Removed [] from assert.

--- gulp-mon-lab/js/compute.js
+++ gulp-mon-lab/js/compute.js
@@ -38,13 +38,27 @@ exports.addFive = addFive;
 function sum(num1, num2) {
   var total = (num1 + num2);
   return total;
-  console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total);
+  // console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total);
 }
 exports.sum = sum;
 
 function multiply(num1, num2) {
   var total = (num1 * num2);
   return total;
-  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + total);
+  // console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + total);
 }
 exports.multiply = multiply;
+
+function sumAndMultiply(num1, num2, num3) {
+  var array = [];
+  var sumFirstTwo = sum(num1, num2);
+  var sumAll = sum(sumFirstTwo, num3);
+  array[0] = sumAll;
+  var multFirstTwo = multiply(num1, num2);
+  var multAll = multiply(multFirstTwo, num3);
+  array[1] = multAll;
+  return array;
+  console.log(num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' +sumAll + '.');
+  console.log('The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of ' + multAll + '.');
+}
+exports.sumAndMultiply = sumAndMultiply;
