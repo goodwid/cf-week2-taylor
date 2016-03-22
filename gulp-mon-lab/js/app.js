89433ba6cebe589ddf0e102387322631ec100235
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-21 17:40:59 -0700

    Corrected pQ variable names and used .textContent to write questions to page

--- gulp-mon-lab/js/app.js
+++ gulp-mon-lab/js/app.js
@@ -2,28 +2,31 @@ var exports = {};
 
 var pQ1  = document.getElementById('question-1');
 var pA1 = document.getElementById('answer-1');
-var pQ1  = document.getElementById('question-2');
-var pA1 = document.getElementById('answer-2');
-var pQ1  = document.getElementById('question-3');
-var pA1 = document.getElementById('answer-3');
-var pQ1  = document.getElementById('question-4');
-var pA1 = document.getElementById('answer-4');
-var pQ1  = document.getElementById('question-5');
-var pA1 = document.getElementById('answer-5');
+var pQ2  = document.getElementById('question-2');
+var pA2 = document.getElementById('answer-2');
+var pQ3  = document.getElementById('question-3');
+var pA3 = document.getElementById('answer-3');
+var pQ4  = document.getElementById('question-4');
+var pA4 = document.getElementById('answer-4');
+var pQ5  = document.getElementById('question-5');
+var pA5 = document.getElementById('answer-5');
 
 var questions = [
-  'Write a function called sum() that takes in two numbers as arguments and then returns the sum of those numbers.',
-  'Write a function called multiply() that takes in two numbers as arguments and then returns the product of those numbers.',
-  'Write a function called sumAndMultiply() that takes in THREE numbers as arguments and then returns two values in an array: the sum of those numbers and the product of those numbers.',
-  'Write a function called sumArray() that takes in an array of numbers as a single argument and then returns the sum of those numbers.',
-  'Write a function called multiplyArray() that takes in an array of numbers as a single argument and then returns the product of those numbers.'
-]
+  'Problem 1: Write a function called sum() that takes in two numbers as arguments and then returns the sum of those numbers.',
+  'Problem 2: Write a function called multiply() that takes in two numbers as arguments and then returns the product of those numbers.',
+  'Problem 3: Write a function called sumAndMultiply() that takes in THREE numbers as arguments and then returns two values in an array: the sum of those numbers and the product of those numbers.',
+  'Problem 4: Write a function called sumArray() that takes in an array of numbers as a single argument and then returns the sum of those numbers.',
+  'Problem 5: Write a function called multiplyArray() that takes in an array of numbers as a single argument and then returns the product of those numbers.'
+];
+
+pQ1.textContent = questions[0];
+pQ2.textContent = questions[1];
+pQ3.textContent = questions[2];
+pQ4.textContent = questions[3];
+pQ5.textContent = questions[4];
 
 
-var arg = '2, 3, 4, 5';
 document.addEventListener('DOMContentLoaded', function() {
 
-pFunctionIn.textContent  = 'Calling multiplyArray() with an argument of ' + arg;
-pFunctionOut.textContent = 'multiplyArray() returned ' + multiplyArray([2,3,4,5]);
 
 });
